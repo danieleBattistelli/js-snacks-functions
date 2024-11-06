05478ab7c52d716dade84e8a385ef6e447b42a16
danieleBattistelli <bomber81@gmail.com>
2024-11-06 18:05:39 +0100

    feat: 05- bONUS

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -10,9 +10,36 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+function saluta(nome) {
+    const oraCorrente = new Date().getHours();
+    let saluto;
+
+    if(oraCorrente<13){
+        saluto = "Buongiorno";
+    }else if(oraCorrente < 17){
+        saluto = "Buon pomeriggio";   
+    }else {
+        saluto = "Buonasera";
+    }
+
+    return `${saluto}, ${nome}`
+}
+
+const saluta2 = (nome) => { const oraCorrente = new Date().getHours(); 
+    let saluto; 
+    if (oraCorrente < 13) { 
+        saluto = "Buongiorno"; 
+    } else if (oraCorrente < 17) { 
+        saluto = "Buon pomeriggio"; 
+    } else { 
+        saluto = "Buonasera"; 
+    } 
+    return `${saluto}, ${nome}!`; 
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(saluta(name))
+console.log(saluta2(name))
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
